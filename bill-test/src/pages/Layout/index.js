4543f509c6825a83cf6c0271cec6fe913548b87d
penguinboy1108 <82563833+penguinboy1108@users.noreply.github.com>
2025-08-23 15:33:51 +1200

    feat: refactor Layout component to include TabBar navigation and remove unused elements

--- bill-test/src/pages/Layout/index.js
+++ bill-test/src/pages/Layout/index.js
@@ -1,33 +1,52 @@
-import { Outlet } from "react-router-dom";
-import { Button } from "antd-mobile";
-import { useEffect } from "react";
-import { useDispatch } from 'react-redux';
-import { getBillList } from "@/store/modules/billStore";
-const Layout = () => {
-  const dispatch = useDispatch();
+import { TabBar } from 'antd-mobile'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline
+} from 'antd-mobile-icons'
+import './index.scss'
 
-  useEffect(() => {
-    // console.log('Fetching bill list...');
-    dispatch(getBillList());
-  }, [dispatch]);
+const tabs = [
+  {
+    key: '/',
+    title: '月度账单',
+    icon: <BillOutline />,
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: '/year',
+    title: '年度账单',
+    icon: <CalculatorOutline />,
+  },
+]
+
+const Layout = () => {
+  const location = useLocation()
+  const navigate = useNavigate()
 
   return (
-    <div>
-      <header>
+    <div className="kaLayout">
+      <div className="page">
+        {/* 二级路由出口 */}
         <Outlet />
-        <h1>My Application</h1>
-      </header>
-      <main>
-        <p>Welcome to my application!</p>
+      </div>
 
-      </main>
-      <Button color="primary">Click Me</Button>
-
-      <footer>
-        <p>© 2023 My Application</p>
-      </footer>
+      <TabBar
+        className="tabbar"
+        activeKey={location.pathname}
+        onChange={key => navigate(key)}
+      >
+        {tabs.map(item => (
+          <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+        ))}
+      </TabBar>
     </div>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
