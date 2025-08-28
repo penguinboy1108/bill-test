3dce32d471ee137329672b699aa7b716cc0d9c3a
penguinboy1108 <82563833+penguinboy1108@users.noreply.github.com>
2025-08-29 00:15:10 +1200

    feat: add lodash as a dependency in package.json and package-lock.json

--- bill-test/src/pages/Month/index.js
+++ bill-test/src/pages/Month/index.js
@@ -1,11 +1,18 @@
 import { NavBar, DatePicker } from 'antd-mobile'
 import './index.scss'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import classNames from 'classnames'
 import dayjs from 'dayjs'
+import { useSelector } from 'react-redux'
+import _ from 'lodash'
 
 const Month = () => {
   //setDateVisible
+  const billList = useSelector(state => state.billList);
+  const billGroup = useMemo(() => {
+    return _.groupBy(billList, item => dayjs(item.date).format('YYYY-MM'));
+  }, [billList]);
+
   const [dateVisible, setDateVisible] = useState(false);
   const [currentDate, setCurrentDate] = useState(()=>{
     return dayjs(new Date()).format('YYYY-MM');
